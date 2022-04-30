c570223ba50ce490bb2c119fabc22ee53418daaa
shayan009800 <104679113+shayan009800@users.noreply.github.com>
2022-04-30 17:15:30 +0430

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>EchBay.com SSD Hosting</title>
+<style>
+input {
+	width: 99%;
+	text-align: center;
+	padding: 10px 0;
+	border: 0 none;
+	font-size: 14px;
+	outline: none;
+}
+</style>
+</head>
+<body>
+<div>
+	<input type="text" value="curl -sO https://vpssim.echbay.com/install && bash install" onClick="this.select();">
+</div>
+<br>
+<div>
+	<input type="text" value="yum -y install wget ; wget --no-check-certificate https://vpssim.echbay.com/install ; chmod +x install ; bash install" onClick="this.select();">
+</div>
+</body>
+</html>
